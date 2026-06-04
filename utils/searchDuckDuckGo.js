6f053bb3025490271736b51ef8ba89c1a9ffd5e7
Chinfeng Chung <chinfeng@users.noreply.github.com>
2026-06-04 09:58:53 +0800

    Add signal option to fetch request in searchDuckDuckGo

--- utils/searchDuckDuckGo.js
+++ utils/searchDuckDuckGo.js
@@ -109,7 +109,7 @@ async function searchDuckDuckGo({ query, language, time_range, pageno, signal })
     const response = await fetch(
       searchUrl,
       {
-        // signal,
+        signal,
         headers: {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
