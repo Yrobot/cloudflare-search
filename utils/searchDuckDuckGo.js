8f9fc1e0dc6b35b5680b1262846c527e9ca2c6ef
Chinfeng Chung <chinfeng@users.noreply.github.com>
2026-06-04 10:12:31 +0800

    Update request headers for DuckDuckGo search

--- utils/searchDuckDuckGo.js
+++ utils/searchDuckDuckGo.js
@@ -113,10 +113,11 @@ async function searchDuckDuckGo({ query, language, time_range, pageno, signal })
         headers: {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-          Accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-          "Accept-Language": "en-US,en;q=0.5",
-          Referer: "https://duckduckgo.com/",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://duckduckgo.com/",
+            "Cookie": "kl=wt-wt; k1=-1",  // 关闭安全搜索等偏好
         },
       }
     );
